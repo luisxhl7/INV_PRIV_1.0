bfdee52bbbc6dc7f560a6ab9a0a221434f04ea46
luis <luisxhl7@gmail.com>
2022-08-02 15:14:54 -0500

    se limpia el codigo de el archivo codigos_php.php

--- view/module/codigos_php.php
+++ view/module/codigos_php.php
@@ -1,58 +1,24 @@
 <?php
-	 
-	if(isset($_POST['buscar'])){ 
+	if(isset($_POST['buscar'])){
     	$Cod_Producto = $_POST['Cod_Producto'];
-    	$valores = array();
-    	$valores['existe'] = "0";
-		$conexion = new mysqli("localhost","root","","invpriv");
 		
-
+		$conexion = new mysqli("localhost","root","","invpriv");
 		$sql = "CALL SpMostrarDatosProducto(".$Cod_Producto.")";
 		$resultados = mysqli_query($conexion, $sql);
 
+    	$valores = array();
+    	$valores['existe'] = "0";
 		while($consulta = mysqli_fetch_array($resultados)){
-		  	$valores['existe'] = "1"; //Esta variable no la usamos en el vídeo (se me olvido, lo siento xD). Aqui la uso en la linea 97 de registro.php
+		  	$valores['existe'] = "1";
 		  	$valores['Nombre'] = $consulta['Nombre'];
 		  	$valores['Precio'] = $consulta['Precio'];
-		  	$valores['Grupo'] = $consulta['Cod_Grupo'];	
+		  	$valores['Grupo'] = $consulta['Cod_Grupo'];
             $valores['Categoria'] = $consulta['Cod_Categoria'];
-		  	$valores['Existencia'] = $consulta['Existencia'];    
+		  	$valores['Existencia'] = $consulta['Existencia']; 
 		}
 
 		sleep(1);
 		$valores = json_encode($valores);
 		echo $valores;
     }
-
-    if(isset($_POST['guardar'])){ 
-    	$doc = $_POST['doc'];
-    	$nombre = $_POST['nombre'];
-    	$dir = $_POST['dir'];
-    	$tel = $_POST['tel'];
-    	$existe = "0";
-
-    	//CONSULTAR
-		$resultados = mysqli_query($conexion,"SELECT * FROM $tabla_db1 WHERE doc = '$doc'");
-		while($consulta = mysqli_fetch_array($resultados)){
-			$existe = "1";
-		}
-
-		if($existe=="1"){
-		  	//actualizar
-		  	$_UPDATE_SQL="UPDATE $tabla_db1 Set 
-			nombre='$nombre', 
-		  	direccion='$dir', 
-		  	telefono='$tel' 
-			  
-		  	WHERE doc='$doc'"; 
-			mysqli_query($conexion,$_UPDATE_SQL); 
-			echo "<b>Dato Actualizado</b>";
-		}else{
-		  	//crear uno nuevo
-		  	mysqli_query($conexion, "INSERT INTO $tabla_db1 (doc,nombre,direccion,telefono) values ('$doc','$nombre','$dir','$tel')");
-			echo "Propietario Agregado";
-		}
-
-    }
-	
-  include("./bd/cerrar_conexion.php");
+?>
