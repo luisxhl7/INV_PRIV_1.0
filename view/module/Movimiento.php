7c6177a28b8d80c9894455fcacfd4907640d992a
luis <luisxhl7@gmail.com>
2022-08-01 23:02:39 -0500

    guardando

--- view/module/Movimiento.php
+++ view/module/Movimiento.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="view/css/FondoInterfazes.css">
         <link rel="stylesheet" href="view/css/movimiento.css">
 
-                            <!-- ajax llenar campos automaticamente  -->
+                        <!-- ajax llenar campos automaticamente  -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     </head>
